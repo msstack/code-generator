823d169f5f4c52e11ad8feb4cc9bab3d08adda5b
randilfernando <randil.fernando@outlook.com>
2018-03-22 15:32:03 +0530

    added dependency from randilfernando/model-converter

--- src/main/java/com/grydtech/msstack/codegenerator/Sample.java
+++ src/main/java/com/grydtech/msstack/codegenerator/Sample.java
@@ -1,15 +1,23 @@
 package com.grydtech.msstack.codegenerator;
 
 import com.grydtech.msstack.modelconverter.business.BusinessModel;
+import com.grydtech.msstack.modelconverter.engine.ModelConverter;
+import com.grydtech.msstack.modelconverter.engine.ModelConverterEntityBased;
+import com.grydtech.msstack.modelconverter.engine.ModelReader;
 import com.grydtech.msstack.modelconverter.engine.ModelReaderJackson;
+import com.grydtech.msstack.modelconverter.microservice.MicroServiceModel;
 
 import java.io.File;
 import java.net.URISyntaxException;
+import java.util.List;
 
 public class Sample {
     public static void main(String[] args) throws URISyntaxException {
         File file = new File(Sample.class.getResource("/sample.json").toURI());
-        BusinessModel businessModel = new ModelReaderJackson().readBusinessModel(file);
+        ModelReader modelReader = new ModelReaderJackson();
+        ModelConverter modelConverter = new ModelConverterEntityBased();
+        BusinessModel businessModel = modelReader.readBusinessModel(file);
+        List<MicroServiceModel> microServiceModels = modelConverter.convertToMicroServiceModel(businessModel);
         System.out.println("Business model read successfully, version: " + businessModel.getVersion());
     }
 }
