e70ff5a55e4cc13b230dfffdf823c953a42c7727
randilfernando <randil.fernando@outlook.com>
2018-03-24 20:59:19 +0530

    remove unwanted test case

--- src/test/java/com/grydtech/msstack/codegenerator/initializer/ProjectInitializerTest.java
+++ src/test/java/com/grydtech/msstack/codegenerator/initializer/ProjectInitializerTest.java
@@ -10,15 +10,15 @@ import java.net.URISyntaxException;
 public class ProjectInitializerTest {
     @Test
     public void testGenerateByInputFile() throws URISyntaxException {
-        File input = new File(ProjectInitializer.class.getResource("/sample.json").toURI());
-        File output = new File("/home/randil/Documents/Projects/Extra/generated/using-freemaker");
-        String groupId = "com.grydtech";
-        String version = "0.1.0";
-
-        try {
-            ProjectInitializer.initializeProject(output, input, groupId, version);
-        } catch (IOException | TemplateException e) {
-            e.printStackTrace();
-        }
+//        File input = new File(ProjectInitializer.class.getResource("/sample.json").toURI());
+//        File output = new File("/home/randil/Documents/Projects/Extra/generated/using-freemaker");
+//        String groupId = "com.grydtech";
+//        String version = "0.1.0";
+//
+//        try {
+//            ProjectInitializer.initializeProject(output, input, groupId, version);
+//        } catch (IOException | TemplateException e) {
+//            e.printStackTrace();
+//        }
     }
 }
